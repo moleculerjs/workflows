38968e409a4da8bbfad13cffff090a0b6452e73d
Icebob <mereg.norbert@gmail.com>
2025-06-18 09:36:49 +0200

    revert context type

--- src/types.ts
+++ src/types.ts
@@ -30,7 +30,7 @@ export interface WorkflowsMiddlewareOptions {
 	tracing?: boolean;
 }
 
-export type WorkflowHandler<P, M, L, H> = (ctx: Context<P, M, L, H>) => Promise<unknown | void>;
+export type WorkflowHandler = (ctx: Context) => Promise<unknown | void>;
 
 export interface CreateJobOptions {
 	jobId?: string;
